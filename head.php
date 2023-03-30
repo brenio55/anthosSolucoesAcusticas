59b68f0e42bfe9242be58b6e51a302b1085a778d
brenio55 <brenio55@outlook.com>
2023-03-30 17:37:47 -0300

    Removido cannonical tag das outras páginas, pois só pode haver uma cannonical tag no site inteiro

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="canonical" href="https://anthosacustica.com.br" />
+    <!-- <link rel="canonical" href="https://anthosacustica.com.br" /> -->
     <title>anthos - soluções acústicas</title>
 
     <meta name="robots" content="index, follow">
