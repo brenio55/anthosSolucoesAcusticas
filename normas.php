f23c28446bbbc4147dcd3f3584fcf050a01b8df0
brenio55 <brenio55@outlook.com>
2023-04-01 13:21:31 -0300

    Adicionada página de normas - apenas PC.

---
+++ normas.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require("head.php"); ?>
+    <?php require("aditionalHead.php"); ?>
+</head>
+<body>
+    <?php require("header.php"); ?>
+    <div class="espacoPadrao"></div>
+    <section class="normas">
+        <h1>Nossas normas de medição e referências</h1>
+
+        <div class="flex spaceBetween">
+            <div class="normasLi">
+                <ul>
+                    <li class="normaTitle">NBR 10151:2019 Errata 1:2020</li>
+                    <li class="normaDesc">Acústica – Medição e avaliação de níveis de pressão sonora
+em áreas habitadas – Aplicação de uso geral</li>
+                    <br>
+                    <li class="normaTitle">NBR 10152:2017 Errata 1:2020</li>
+                    <li class="normaDesc">Acústica – Níveis de pressão sonora em ambientes internos
+a edificações</li>
+                    <br>
+                    <li class="normaTitle">NBR ISO 3382-2:2017</li>
+                    <li class="normaDesc">Acústica – Medição de parâmetros de acústica de salas –
+Parte 2: Tempo de reverberação em salas comuns</li>
+                    <br>
+                    <li class="normaTitle">NBR 16313:2014</li>
+                    <li class="normaDesc">Acústica – Terminologia</li>
+                    <br>
+                    <li class="normaTitle">NBR 15575 – Partes 1 a 6</li>
+                    <li class="normaDesc">Edificações habitacionais – Desempenho</li>
+                    <br>
+                    <li class="normaTitle">NBR ISO/IEC 17025:2017</li>
+                    <li class="normaDesc">Requisitos gerais para a competência de laboratórios de
+ensaio e calibração.</li>
+                </ul>
+            </div>
+            <div class="livrosDasNormas">            
+                <ol>                    
+                    <h4 class="TitleLivros">LIVROS UTILIZADOS COMO REFERÊNCIAS PELA NOSSA EQUIPE</h4>
+
+                    <li>THE BOOK OF NOISE CONTROL</li>
+                    <li>McGraw Hill Higher Education, 2ª Edição (1 janeiro de 1979)</li>
+                    <li>Building Acoustics - Tor Erik Virgran</li>
+                    <li>Architectural Acoustics - Marshall Long</li>
+                    <li>Master Handbook of Acoustics - Ken C. Pohlmann e F. Alton Everest</li>
+                    <li>Acústica Aplicada ao Controle do Ruído - Sylvio R. Bistafa</li>
+                    <li>Sinais e Sistemas - Alan V. Oppenheim e Alan S. Willsky</li>
+                    <li>Acústica de Salas - Projeto e Modelagem - Eric Brandão</li>
+                    <li>Acústica nos Edifícios - Jorge Patrício</li>
+                </ol>
+            </div>
+
+        </div>
+        
+    </section>
+    <?php require("footer.php"); ?>
+</body>
+</html>
